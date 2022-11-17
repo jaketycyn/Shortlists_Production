3f20ee11cde08bd67849d3fa5f4b50d565c2b66d
Jake TyCyn <jaketycyn@gmail.com>
2022-11-17 15:07:01 -0800

    docs _Example.tsx component

---
+++ src/components/_Example.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import  Link from "next/link";
+
+//import { useAppContext } from "../context/appContext";
+
+const Example: NextPage = () => {
+  //const [showAdd, setShowAdd ] = React.useState<boolean>(false);
+
+  return (
+    <div className="h-14 grid grid-rows-1 grid-cols-3 w-full text-center justify-center items-center  ">
+      {/* exmaple content and divs in here */}
+    </div>
+  );
+};
+
+export default FooterNav;
