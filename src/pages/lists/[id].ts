5b72a762ae779883cf05f60e34e00ce0d285efb1
Jake TyCyn <jaketycyn@gmail.com>
2022-11-29 09:30:43 -0800

    feat - nextJS router base for lists

---
+++ src/pages/lists/[id].ts
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const ListPage: NextPage = () => {
+  const data = console.log("data");
+  return (
+    <div>
+      <h1>Hi in list page</h1>
+    </div>
+  );
+};
+
+export default ListPage;
