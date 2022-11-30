dab62461c8cf5d0f26562e7c1c7e4d0fba53c204
Jake TyCyn <jaketycyn@gmail.com>
2022-11-29 20:42:05 -0800

    feat - redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 import counterReducer from "../slices/counterSlice";
+import listSlice from "../slices/listSlice";
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+// Redux-Persist setup
+// https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+//place reducers here
+const rootReducer = combineReducers({
+  list: listSlice,
+  counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
