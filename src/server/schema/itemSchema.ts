db465c0912a3c8a44b034b6882c69bcb8a095868
Jake TyCyn <jaketycyn@gmail.com>
2022-11-30 09:26:16 -0800

    refactor - updateList

--- src/server/schema/itemSchema.ts
+++ src/server/schema/itemSchema.ts
@@ -9,5 +9,10 @@ export const deleteItemSchema = z.object({
   userId: z.string(),
 });
 
+export const updateItemSchema = deleteItemSchema.extend({
+  title: z.string(),
+});
+
 export type AddItemSchema = z.TypeOf<typeof addItemSchema>;
 export type DeleteItemSchema = z.TypeOf<typeof deleteItemSchema>;
+export type UpdateItemSchema = z.TypeOf<typeof updateItemSchema>;
