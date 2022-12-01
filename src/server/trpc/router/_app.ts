2042877779183fa56982c66c6682a131d5da2f93
Jake TyCyn <jaketycyn@gmail.com>
2022-11-30 18:26:29 -0800

    remove - example Router + schema

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,13 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { userRouter } from "./user";
 import { userItemRouter } from "./userItem";
 import { userListRouter } from "./userList";
 
 export const appRouter = router({
   auth: authRouter,
-  example: exampleRouter,
   user: userRouter,
   userItem: userItemRouter,
   userList: userListRouter,
