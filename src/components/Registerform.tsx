b32e04951073c8dbae99f2a9c169f4e0ade35897
Jake TyCyn <jaketycyn@gmail.com>
2022-12-01 00:16:22 -0800

    fix - registerForm background white

--- src/components/Registerform.tsx
+++ src/components/Registerform.tsx
@@ -59,7 +59,7 @@ const RegisterForm: NextPage = () => {
 
   return (
     <div className="mt-20 flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
-      <div className="w-full max-w-md space-y-8">
+      <div className="w-full max-w-md space-y-8 bg-white">
         <div>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Register a new account
