0e592eb9b867a6a90e9ba546fb43c32febcdcfdd
Jake TyCyn <jaketycyn@gmail.com>
2022-12-03 20:59:09 -0800

    docs - old components

---
+++ src/components/old/OldComponents.tsx
@@ -0,0 +1,24 @@
+export const OldComponents = () => {
+  return <>Oldies</>;
+};
+//? OLD Mylists display: Start */
+/* 
+                  <button
+                    className={`h-30 mt-12 grid  w-1/2 grid-cols-4 grid-rows-1 items-center rounded-lg border-2 border-black/80 bg-white text-center font-semibold ${
+                      !userListsOpen && "bg-accent"
+                    }`}
+                    onClick={() => setUserListsOpen(!userListsOpen)}
+                  >
+                    <HiOutlineChevronRight
+                      //index + 1 needed because for some reason index at 0 was never found even with it being hard coded in.
+                      className={`col-span-1 col-start-1 row-span-1 row-start-1 m-2 grid ${
+                        userListsOpen && "rotate-90"
+                      } `}
+                    />
+
+                    <h1 className="col-span-2 col-start-2 row-span-1 row-start-1 grid">
+                      My lists
+                    </h1>
+                  </button> */
+
+/* //? OLD Mylists display: End */
