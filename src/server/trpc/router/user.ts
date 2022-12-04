e8e7b36442f296d08906d068b8a43bb68fb12cb6
Jake TyCyn <jaketycyn@gmail.com>
2022-12-04 14:33:51 -0800

    feat - findUser + getAllUsers

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -2,7 +2,8 @@ import { TRPCError } from "@trpc/server";
 import { registerSchema } from "../../schema/userSchema";
 import { hash } from "argon2";
 
-import { router, publicProcedure } from "../trpc";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { z } from "zod";
 
 export const userRouter = router({
   //! examples
@@ -16,6 +17,36 @@ export const userRouter = router({
   //   getAll: publicProcedure.query(({ ctx }) => {
   //     return ctx.prisma.example.findMany();
   //   }),
+  findUser: protectedProcedure
+    .input(z.object({ email: z.string().email() }))
+    .mutation(async ({ input, ctx }) => {
+      const { email } = input;
+      const userMatch = await ctx.prisma.user.findUnique({
+        where: { email },
+      });
+      return {
+        status: 201,
+        message: "User Matches successfully",
+        result: {
+          user: userMatch,
+        },
+      };
+    }),
+  getAllUsers: protectedProcedure.query(async ({ ctx }) => {
+    const results = await ctx.prisma.user.findMany({
+      select: {
+        email: true,
+        username: true,
+        id: true,
+      },
+    });
+
+    return {
+      status: 201,
+      message: "Retrieved All Users",
+      results,
+    };
+  }),
   registerUser: publicProcedure
     .input(registerSchema)
     .mutation(async ({ input, ctx }) => {
@@ -43,7 +74,11 @@ export const userRouter = router({
       return {
         status: 201,
         message: "Account created successfully",
-        result: { email: result.email, id: result.id },
+        result: {
+          email: result.email,
+          id: result.id,
+          userName: result.username,
+        },
       };
     }),
 });
