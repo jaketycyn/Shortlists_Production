94810b99376ac084861333f8359183f17ac7b048
Jake TyCyn <jaketycyn@gmail.com>
2022-12-04 18:31:14 -0800

    feat - search friends prelevi

--- src/components/layouts/profilepage/ProfilePageLayout.tsx
+++ src/components/layouts/profilepage/ProfilePageLayout.tsx
@@ -24,6 +24,8 @@ const ProfilePageLayout: NextPage = () => {
   //console.log("foundUsers: ", foundUsers);
   //store all Users in DB
 
+  const { handleSubmit, register } = useForm<any>();
+
   useEffect(() => {
     if (foundUsers) {
       const users = foundUsers.results;
@@ -176,7 +178,12 @@ const ProfilePageLayout: NextPage = () => {
                     id="friend-search"
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 "
                     placeholder="Search for Friends..."
-                    onChange={(e) => searchUsers(e.target.value)}
+                    {...register("test", {
+                      onChange: (e) => {
+                        searchUsers(e.target.value);
+                      },
+                      onBlur: (e) => {},
+                    })}
                   />
                   <button
                     type="submit"
@@ -185,6 +192,49 @@ const ProfilePageLayout: NextPage = () => {
                     Search
                   </button>
                   {/* Display Users from Search */}
+                  <div>
+                    {filteredResults ? (
+                      <div className="flex flex-col">
+                        <ul className="divide-y divide-gray-200">
+                          {filteredResults.map((user, key) => (
+                            <div className="" key={key}>
+                              <li className="flex flex-row items-center py-3 sm:py-4">
+                                <div className="mr-2 flex-shrink-0">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="h-6 w-6"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                                    />
+                                  </svg>
+
+                                  {/* 
+                        Image Placeholder
+                      <Image className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image"> */}
+                                </div>
+                                <div className="min-w-0 flex-1">
+                                  <p className="truncate text-sm font-medium ">
+                                    {user.username}
+                                  </p>
+                                  <p className="truncate text-sm ">
+                                    {user.email}
+                                  </p>
+                                  {/* <p className="truncate text-sm ">{user.status}</p> */}
+                                </div>
+                              </li>
+                            </div>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null}
+                  </div>
                 </div>
               </form>
               {/* Display All Users - Will change to friends later */}
