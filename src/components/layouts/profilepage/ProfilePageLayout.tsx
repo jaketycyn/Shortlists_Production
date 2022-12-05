41b8110909c6842f9ac36ce6f72edddc7816ef4c
Jake TyCyn <jaketycyn@gmail.com>
2022-12-04 18:34:19 -0800

    fix - search Icon

--- src/components/layouts/profilepage/ProfilePageLayout.tsx
+++ src/components/layouts/profilepage/ProfilePageLayout.tsx
@@ -156,7 +156,7 @@ const ProfilePageLayout: NextPage = () => {
           <div>
             <ul className="mt-2 mb-2 divide-y-8 divide-gray-200">
               <form onSubmit={() => console.log("submitting")}>
-                <div className="relative">
+                <div className="relative flex flex-col">
                   <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -182,7 +182,6 @@ const ProfilePageLayout: NextPage = () => {
                       onChange: (e) => {
                         searchUsers(e.target.value);
                       },
-                      onBlur: (e) => {},
                     })}
                   />
                   <button
@@ -191,50 +190,50 @@ const ProfilePageLayout: NextPage = () => {
                   >
                     Search
                   </button>
-                  {/* Display Users from Search */}
-                  <div>
-                    {filteredResults ? (
-                      <div className="flex flex-col">
-                        <ul className="divide-y divide-gray-200">
-                          {filteredResults.map((user, key) => (
-                            <div className="" key={key}>
-                              <li className="flex flex-row items-center py-3 sm:py-4">
-                                <div className="mr-2 flex-shrink-0">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="h-6 w-6"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                  </svg>
+                </div>
+                {/* Display Users from Search */}
+                <div>
+                  {filteredResults ? (
+                    <div className="flex flex-col">
+                      <ul className="divide-y divide-gray-200">
+                        {filteredResults.map((user: any, key: any) => (
+                          <div className="" key={key}>
+                            <li className="flex flex-row items-center py-3 sm:py-4">
+                              <div className="mr-2 flex-shrink-0">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="h-6 w-6"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                                  />
+                                </svg>
 
-                                  {/* 
+                                {/* 
                         Image Placeholder
                       <Image className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image"> */}
-                                </div>
-                                <div className="min-w-0 flex-1">
-                                  <p className="truncate text-sm font-medium ">
-                                    {user.username}
-                                  </p>
-                                  <p className="truncate text-sm ">
-                                    {user.email}
-                                  </p>
-                                  {/* <p className="truncate text-sm ">{user.status}</p> */}
-                                </div>
-                              </li>
-                            </div>
-                          ))}
-                        </ul>
-                      </div>
-                    ) : null}
-                  </div>
+                              </div>
+                              <div className="min-w-0 flex-1">
+                                <p className="truncate text-sm font-medium ">
+                                  {user.username}
+                                </p>
+                                <p className="truncate text-sm ">
+                                  {user.email}
+                                </p>
+                                {/* <p className="truncate text-sm ">{user.status}</p> */}
+                              </div>
+                            </li>
+                          </div>
+                        ))}
+                      </ul>
+                    </div>
+                  ) : null}
                 </div>
               </form>
               {/* Display All Users - Will change to friends later */}
