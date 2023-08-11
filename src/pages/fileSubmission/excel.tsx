a9a9cbf19c2a495a07af509fccc2d9952f7819ca
Jake TyCyn <jaketycyn@gmail.com>
2023-08-10 18:54:11 -0700

    Excel Update

--- src/pages/fileSubmission/excel.tsx
+++ src/pages/fileSubmission/excel.tsx
@@ -1,35 +1,56 @@
 import { useState } from "react";
+import Papa from "papaparse";
+import * as XLSX from "xlsx";
 
 const Excel = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleFileChange = (e: any) => {
-    setFile(e.target.files[0]);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files ? e.target.files[0] : null;
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
   };
 
   const handleSubmit = async () => {
     if (!file) return;
+
     const reader = new FileReader();
-    reader.onload = async (e: any) => {
-      try {
-        const json = JSON.parse(e.target.result);
-        // await addMoviesAndGenres.mutate(json);
-        alert("Data added succesfully!");
-      } catch (err: any) {
-        console.error("Error parsing JSON", err.message, err.name);
-        alert("Error processing file. Make sure it is a valid JSON");
+
+    reader.onload = async (e: ProgressEvent<FileReader>) => {
+      if (e.target?.result) {
+        const data = e.target.result;
+        try {
+          // Parse CSV
+          if (file.type === "text/csv") {
+            Papa.parse(data.toString(), {
+              complete: (parsedData) => {
+                console.log("parsedData", parsedData);
+              },
+            });
+          }
+          // TODO: Handle other types e.g., Excel
+        } catch (err: any) {
+          console.error("Error parsing JSON", err.message);
+          alert("Error processing file. Make sure it is a valid JSON or CSV");
+        }
       }
     };
+
+    if (file.type === "text/csv") {
+      reader.readAsText(file);
+    }
+    // TODO: Add reading methods for other file types, e.g., Excel
   };
 
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="mx-auto flex flex-col items-center justify-center p-4 pt-20">
       <input type="file" onChange={handleFileChange} className="mb-4" />
       <button
         onClick={handleSubmit}
         className="bg-blue-500 px-4 py-2 text-white"
       >
-        Upload
+        Upload Excel Document
       </button>
     </div>
   );
