28962bde8858fa7796c9254863dd0106ee6c1458
Jake TyCyn <jaketycyn@gmail.com>
2023-08-22 15:11:31 -0700

    Error Boundary update test

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -4,6 +4,7 @@ import FooterNav from "./navigation/FooterNav";
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  errorCount: number;
 }
 
 interface ErrorBoundaryProps {
@@ -19,12 +20,12 @@ class ErrorBoundary extends React.Component<
     super(props);
 
     // Define a state variable to track whether there is an error or not
-    this.state = { hasError: false };
+    this.state = { hasError: false, errorCount: 0 };
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     // Update state so the next render will show the fallback UI
-    return { hasError: true };
+    return { hasError: true, errorCount: 0 };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
@@ -47,21 +48,26 @@ class ErrorBoundary extends React.Component<
               className="mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
               type="button"
               onClick={() => {
-                this.setState({ hasError: false }, () => {
-                  router.push("/"); // Navigate back to home
-                });
+                this.setState(
+                  (prevState) => ({
+                    hasError: false,
+                    errorCount: prevState.errorCount + 1,
+                  }),
+                  () => {
+                    router.push("/"); // Navigate back to home
+                  }
+                );
               }}
             >
               Back to Home
             </button>
           </div>
-          <FooterNav />
         </div>
       );
     }
 
     // Return children components in case of no error
-    return this.props.children;
+    return <div key={this.state.errorCount}>{this.props.children}</div>;
   }
 }
 
