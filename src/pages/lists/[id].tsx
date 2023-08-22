57f664f2ef71a9b8897fdade1a633fdcc5e594d6
Jake TyCyn <jaketycyn@gmail.com>
2023-08-22 13:22:38 -0700

    debugging

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -225,12 +225,13 @@ const ListPage: NextPage = () => {
   const debouncedSearchTerm: string = useDebounce<string>(searchTerm, 500);
   //console.log("debouncedSearchTerm: ", debouncedSearchTerm);
 
-  console.log("About to filter:", currentUser);
+  console.log("About to filter: currentUser");
   const currentUser = users?.filter((i) => i.id === session?.user?.id);
-  console.log("About to filter:", usersNotCurrent);
+  console.log("currentUser: ," + JSON.stringify(currentUser, 0, 2));
+  console.log("About to filter: usersNotCurrent");
   const usersNotCurrent = users?.filter((i) => i.id !== session?.user?.id);
 
-  //console.log("usersNotCurrent: ," + JSON.stringify(usersNotCurrent, 0, 2));
+  console.log("usersNotCurrent: ," + JSON.stringify(usersNotCurrent, 0, 2));
 
   useEffect(() => {
     if (debouncedSearchTerm) {
@@ -244,7 +245,7 @@ const ListPage: NextPage = () => {
       );
 
       //console.log("newFilteredUsers: ," + JSON.stringify(newFilteredUsers, 0, 2));
-      console.log("About to filter:", filteredResults);
+      console.log("About to filter: filteredResults");
       const filteredUsers = usersNotCurrentWOIdEmail?.filter((user) => {
         return Object.values(user)
           .join("")
@@ -256,14 +257,14 @@ const ListPage: NextPage = () => {
 
       //? https://stackoverflow.com/questions/31005396/filter-array-of-objects-with-another-array-of-objects
       //setResults from a filtering of usernames from usersNotCurrentWOIdEmail but including emails/ids
-      console.log("About to filter:", newFilteredResults);
+      console.log("About to filter: newFilteredResults");
       const newFilteredResults = usersNotCurrent?.filter((el) => {
         return filteredUsers?.some((u) => {
           return u.username === el.username;
         });
       });
-      console.log("newFilteredResults: ", newFilteredResults);
-
+      console.log("newFilteredResults:  newFilteredResults");
+      console.log("filteredResults: ", filteredResults);
       setFilteredResults(newFilteredResults);
     } else {
       setFilteredResults([]);
