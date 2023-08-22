e0d6400c40c0efc90e573f5788f06b41d591b050
Jake TyCyn <jaketycyn@gmail.com>
2023-08-22 15:27:32 -0700

    UP: Errorboundary w/ redux

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,42 +1,31 @@
 import React, { ErrorInfo } from "react";
+import { connect } from "react-redux";
 import { withRouter, NextRouter } from "next/router";
-import FooterNav from "./navigation/FooterNav";
-
-interface ErrorBoundaryState {
-  hasError: boolean;
-  errorCount: number;
-}
+import { setError, clearError } from "../slices/errorSlice";
 
 interface ErrorBoundaryProps {
   children: React.ReactNode;
   router: NextRouter;
+  hasError: boolean;
+  setError: () => void;
+  clearError: () => void;
 }
 
-class ErrorBoundary extends React.Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-
-    // Define a state variable to track whether there is an error or not
-    this.state = { hasError: false, errorCount: 0 };
+class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+  static getDerivedStateFromError(error: Error) {
+    this.props.setError(); //set global error state to true
+    return {};
   }
 
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    // Update state so the next render will show the fallback UI
-    return { hasError: true, errorCount: 0 };
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    // You can also log the error to an error reporting service
+    console.error(error, errorInfo);
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    // You can use your own error logging service here
-    console.log({ error, errorInfo });
-  }
-
-  render(): React.ReactNode {
-    const { router } = this.props;
+  render() {
+    const { router, clearError, hasError } = this.props;
     // Check if the error is thrown
-    if (this.state.hasError) {
+    if (hasError) {
       // You can render any custom fallback UI
       return (
         <div className="flex min-h-screen items-center justify-center bg-blue-500">
@@ -48,15 +37,8 @@ class ErrorBoundary extends React.Component<
               className="mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
               type="button"
               onClick={() => {
-                this.setState(
-                  (prevState) => ({
-                    hasError: false,
-                    errorCount: prevState.errorCount + 1,
-                  }),
-                  () => {
-                    router.push("/"); // Navigate back to home
-                  }
-                );
+                clearError(); // Clear global error state
+                router.push("/");
               }}
             >
               Back to Home
@@ -67,8 +49,19 @@ class ErrorBoundary extends React.Component<
     }
 
     // Return children components in case of no error
-    return <div key={this.state.errorCount}>{this.props.children}</div>;
+    return this.props.children;
   }
 }
 
-export default withRouter(ErrorBoundary);
+const mapStateToProps = (state: any) => ({
+  hasError: state.error.hasError,
+});
+
+const mapDispatchToProps = {
+  setError,
+  clearError,
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(ErrorBoundary)
+);
