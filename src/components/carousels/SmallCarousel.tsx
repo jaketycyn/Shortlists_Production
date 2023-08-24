a27a9981f2f9f1e9cd1f75ef76a217f4b1e299fb
Jake TyCyn <jaketycyn@gmail.com>
2023-08-23 21:38:41 -0700

    UP: SM carousel w/ framer motion

--- src/components/carousels/SmallCarousel.tsx
+++ src/components/carousels/SmallCarousel.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface CarouselProps {
-  children: React.ReactNode;
+  children: React.ReactNode[];
 }
 
-const SmallCarousel = React.forwardRef(
-  ({ children }: CarouselProps, ref: React.Ref<HTMLDivElement>) => {
-    return (
-      <div
-        ref={ref}
-        className="scrollbar-hide flex overflow-x-auto bg-green-400"
-        data-type="carousel"
-      >
-        {children}
-      </div>
+const SmallCarousel = ({ children }: CarouselProps) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % children.length);
+  };
+
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? children.length - 1 : prevIndex - 1
     );
-  }
-);
+  };
+
+  return (
+    <div className="relative flex w-full flex-row items-center justify-center overflow-hidden bg-green-400">
+      <button onClick={handlePrev} className="pr-4">
+        Prev
+      </button>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={activeIndex}
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -50 }}
+          transition={{ duration: 0.3 }}
+        >
+          {children[activeIndex]}
+        </motion.div>
+      </AnimatePresence>
+      <button onClick={handleNext} className="pl-4">
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default SmallCarousel;
