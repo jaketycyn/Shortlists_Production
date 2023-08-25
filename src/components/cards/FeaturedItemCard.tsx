8618d3f3104cfa28d58677ff0109a6a3c4f6f57d
Jake TyCyn <jaketycyn@gmail.com>
2023-08-25 09:55:38 -0700

    BF: Removed empty function for deployment

--- src/components/cards/FeaturedItemCard.tsx
+++ src/components/cards/FeaturedItemCard.tsx
@@ -22,20 +22,6 @@ interface FeaturedItemCardProps {
   featuredLists: ListType[];
 }
 
-// alternating colors for the cards
-//   const colors = [
-//     "bg-red-300",
-//     "bg-blue-300",
-//     "bg-yellow-300",
-//     "bg-green-300",
-//     "bg-indigo-300",
-//     "bg-purple-300",
-//   ];
-
-//   const bgColor = colors[index % 6];
-
-const filterItemsBylist = () => {};
-
 const FeaturedItemCard = ({
   title,
   index,
